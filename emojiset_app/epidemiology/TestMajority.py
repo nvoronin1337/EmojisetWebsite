29307e962985c7049d639acfb2b939d6a1be7e4b
nvoronin1337 <voro0612@gmail.com>
2020-12-08 22:33:54 +0300

    fix for networkx

--- emojiset_app/epidemiology/TestMajority.py
+++ emojiset_app/epidemiology/TestMajority.py
@@ -5,26 +5,16 @@ Created on Mon Dec 7 09:45:49 2020
 @author: Sancho, Martin, Edwin
 """
 
-import networkx as nx
-import ndlib.models.ModelConfig as mc
-from emojiset_app.epidemiology.MultipleMajority import MultipleMajority
-from bokeh.io import show, output_file, save
-from ndlib.viz.bokeh.DiffusionTrend import DiffusionTrend
-#rom bs4 import BeautifulSoup
-from emojiset_app.utils import debug
-from bs4 import BeautifulSoup
-import codecs
+def MMR(bias, fraction_Adopter, fraction_Rejector, Q):
+    import networkx as nx
+    import ndlib.models.ModelConfig as mc
+    from emojiset_app.epidemiology.MultipleMajority import MultipleMajority
+    from bokeh.io import show, output_file, save
+    from ndlib.viz.bokeh.DiffusionTrend import DiffusionTrend
+    from emojiset_app.utils import debug
+    from bs4 import BeautifulSoup
+    import codecs
 
-#Network Topology
-#graph = nx.erdos_renyi_graph(1000, .8)
-#graph = nx.dense_gnm_random_graph(1000, 5000)
-#graph = nx.random_regular_graph(5, 1000)
-#graph = nx.barabasi_albert_graph(1000, 5)
-#graph = nx.powerlaw_cluster_graph(1000, 5, 0.2)
-#graph = nx.duplication_divergence_graph(1000, 0.2)
-#model selection
-
-def MMR(bias ,fraction_Adopter, fraction_Rejector, Q):
     graph = nx.watts_strogatz_graph(1000, 5, .2)
     denom = bias;
     model = MultipleMajority(graph, denom)
